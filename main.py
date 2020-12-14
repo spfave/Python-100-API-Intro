fd0d4c002bfb89c47c20dfcf3fc8336ea210aac1
SebastianF <sebpfave2@gmail.com>
2020-12-13 21:59:26 -0500

    getting data

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import requests
 
 # Main
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
+
+data = response.json()["iss_position"]["latitude"]
+print(data)
